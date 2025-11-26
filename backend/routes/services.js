c6171e3a6ba1fd510014d37e2e42531776d67642
jrKruger89 <kruger89@hotmail.com>
2025-11-26 13:52:31 +0100

    feat: Add API endpoints for businesses and services with basic route structure

---
+++ backend/routes/services.js
@@ -0,0 +1,39 @@
+// API endpoints for services
+
+/* ---------- GET routes ---------- */
+
+// GET all services
+app.get("/api/services", async (req, res) => {
+	try {
+	} catch (error) {}
+});
+
+// GET specific service using id
+app.get("/api/services/:id", async (req, res) => {
+	try {
+	} catch (error) {}
+});
+
+/* ---------- POST routes ---------- */
+
+// POST new service to database
+app.post("/api/services", async (req, res) => {
+	try {
+	} catch (error) {}
+});
+
+/* ---------- PATCH routes ---------- */
+
+// PATCH specific service using id
+app.patch("/api/services/:id", async (req, res) => {
+	try {
+	} catch (error) {}
+});
+
+/* ---------- DELETE routes ---------- */
+
+// DELETE specifik service using id
+app.delete("/api/services/:id", async (req, res) => {
+	try {
+	} catch (error) {}
+});
