356278dbf8282d37f556e243b6f381f8ea525329
Willow <98649380+WeeWillow@users.noreply.github.com>
2025-12-12 15:22:44 +0100

    refactor: Enhance dashboard layout and add navigation links
    Updated the dashboard page to include a header with icons, sections for stories, collaboration requests, and notes feed. Added a 'Create +' button using NavLink for navigation to the post creation page. Improved code formatting and consistency.

--- frontend/app/routes/dashboard.jsx
+++ frontend/app/routes/dashboard.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { useNavigate, NavLink } from "react-router";
 import { useEffect } from "react";
 import ProtectedRoute from "../components/ProtectedRoute";
 import { supabase } from "../library/supabase";
@@ -15,7 +15,7 @@ export default function DashboardPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const {data: {subscription}} = supabase.auth.onAuthStateChange((e) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((e) => {
       if (e === "SIGNED_OUT") {
         navigate("/auth?step=login");
       }
@@ -29,8 +29,29 @@ export default function DashboardPage() {
 
   return (
     <ProtectedRoute>
-      <h1>This is das Board 👍</h1>
-      <Button onClick={handleLogout}>Logout</Button>
+      <header className="dashboard-header">Linup [search icon] | [search icon] | [notification icon] | [burgermenu icon]</header>
+
+      <div>
+        <h1>This is das Board 👍</h1>
+        <Button onClick={handleLogout}>Logout</Button>
+      </div>
+
+      <section className="stories-container">There are stories here</section>
+
+      <section className="collab-requests-container">
+        <h5>Collaboration requests</h5>
+        <section className="collab-feed"></section>
+      </section>
+
+      <section className="notes-feed-container">
+        <h3>Notes feed is here</h3>
+      </section>
+
+      <button >
+        <NavLink to="create-posts">
+          Create +
+        </NavLink>
+      </button>
     </ProtectedRoute>
   );
 }
